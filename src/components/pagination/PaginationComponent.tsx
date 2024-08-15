d7ca2b891020fc010b637f1d3c5db3669e0f84d3
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-15 05:14:34 +0300

    Changed pagination

--- src/components/pagination/PaginationComponent.tsx
+++ src/components/pagination/PaginationComponent.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-
 import {
     Pagination,
     PaginationContent,
@@ -10,45 +9,58 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination";
-import {usePathname} from "next/navigation";
+import {usePathname, useSearchParams} from "next/navigation";
 
 
 const PaginationComponent = ({page, totalPages}) => {
     let path = usePathname();
-    console.log(path);
+    totalPages>500 ? totalPages = 500: totalPages;
+    console.log(`path ${path}`);
+    const searchParams = useSearchParams();
+    const createPageURL = (pageNumber: number | string) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', pageNumber.toString());
+        console.log(`${path}?${params.toString()}`);
+        return `${path}?${params.toString()}`;
+    };
     return (
         <Pagination>
             <PaginationContent>
                 {page > 1 && (
                     <PaginationItem>
-                    <PaginationPrevious href={`${path}?page=${page - 1}`} />
+                    <PaginationPrevious href={`${createPageURL(page - 1)}`} />
+                    {/*<PaginationPrevious href={`${path}?page=${page - 1}`} />*/}
                 </PaginationItem>)
                 }
                 {page > 1 && (
                 <PaginationItem>
-                    <PaginationLink href={`${path}?page=${page-1}`} >
+                    <PaginationLink href={`${createPageURL(page - 1)}`} >
+                    {/*<PaginationLink href={`${path}?page=${page-1}`} >*/}
                         {page-1}
                     </PaginationLink>
                 </PaginationItem>)
                 }
                 <PaginationItem>
-                    <PaginationLink href={`${path}?page=${page}`} isActive>
+                    <PaginationLink href={`${createPageURL(page)}`} isActive>
+                    {/*<PaginationLink href={`${path}?page=${page}`} isActive>*/}
                         {page}
                     </PaginationLink>
                 </PaginationItem>
                 { page < totalPages && (
                 <PaginationItem>
-                    <PaginationLink  href={`${path}?page=${page+1}`}>{page +1}</PaginationLink>
+                    <PaginationLink  href={`${createPageURL(page + 1)}`}>{page +1}</PaginationLink>
+                    {/*<PaginationLink  href={`${path}?page=${page+1}`}>{page +1}</PaginationLink>*/}
                 </PaginationItem>)
                 }
-                { page < totalPages-1 && (
+                { page < totalPages-2 && totalPages<498 && (
                 <PaginationItem>
                     <PaginationEllipsis />
                 </PaginationItem>)
                 }
                 { page < totalPages && (
                     <PaginationItem>
-                        <PaginationNext href={`${path}?page=${page + 1}`} />
+                        <PaginationNext href={`${createPageURL(page + 1)}`} />
+                        {/*<PaginationNext href={`${path}?page=${page + 1}`} />*/}
                     </PaginationItem>)
                 }
             </PaginationContent>
