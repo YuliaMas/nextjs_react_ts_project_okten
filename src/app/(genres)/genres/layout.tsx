3f18087d87a3d0e0c87f474afc4b1f6615fb9c51
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-16 11:15:47 +0300

    Changed styles

--- src/app/(genres)/genres/layout.tsx
+++ src/app/(genres)/genres/layout.tsx
@@ -8,7 +8,7 @@ export default async function Layout({ children }: { children: React.ReactNode})
         <div className="flex  flex-col md:flex-row items-center md:items-start">
             <div>
                 <h1 className={"text-2xl mb-3 md:mb-0 md:text-6xl text-sky-600 text-center"}>Genres</h1>
-                <div className="flex w-2/12 flex-col pe-6 md:p-12">
+                <div className="flex w-2/12 flex-col pe-6 md:p-12 lg:p-6">
                     {
                         genres.map((genre) => (
                             <GenreComponent key={genre.id} genre={genre}/>
