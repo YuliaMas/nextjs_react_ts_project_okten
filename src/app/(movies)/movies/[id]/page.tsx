530c2d818cda1543c8993f4e51c002f6eb454ece
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 00:40:35 +0300

    Add styles

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -9,12 +9,11 @@ type Params = {
 const MoviesListPage = async ({params}: Params) => {
     let infoMovie = await movieService.getMovieById(params.id);
     let videoData = await videoService.getAllTrailers(params.id);
-    console.log(videoData);
     return (
         <div className={"items-center m-5 md:m-0 flex flex-col justify-center"}>
             <h1 className={"text-cyan-500 text-center"}>Info Movie</h1>
             <MoviesListCard infoMovie={infoMovie}/>
-            <div className={"w-full"}>
+            <div className={"w-full mb-8 mt-2"}>
                 <div  className={"flex justify-evenly flex-wrap gap-5"}>
                     <VideoComponent videoData={videoData} />
                 </div>
