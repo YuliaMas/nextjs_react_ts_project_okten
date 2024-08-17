b9b561fb499a7e69202ec84089e134c1bf9cc0cf
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-17 03:59:53 +0300

    Changed Logo img

--- src/components/MovieComponent.tsx
+++ src/components/MovieComponent.tsx
@@ -21,8 +21,8 @@ const MovieComponent:FC<IProps> = ({movie}) => {
                                            priority/>
                                 </div>
                         ) : ( <div className={"contain-content  flex justify-items-center rounded-2xl border-8 border-b-cyan-900  lg:w-64 min-h-80 max-w-80 max-h-80"} >
-                                <Image src="/MovieApp1.jpg"
-                                       className={"border-b-cyan-900 border max-w-80 max-h-80 md:w-42 w-64 lg:w-58 lg:min-w-24 opacity-50 bg-white "}
+                                <Image src="/LogoDog.png"
+                                       className={"border-b-cyan-900 border max-w-80 max-h-80 md:w-42 w-64 lg:w-58 lg:min-w-24 opacity-70 bg-amber-200"}
                                        alt={movie.title}
                                        width={300}
                                        height={300}
