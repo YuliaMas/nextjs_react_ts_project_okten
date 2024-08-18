31afc1f0df0d2217d2010b4e18e987756e809da4
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 04:52:44 +0300

    Update AuthComponent.tsx

--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -7,24 +7,19 @@ const AuthComponent = async () => {
     let status = await authService.getUser();
     if(status.success) console.log('true');
     return (
-        // <ul className={"p-28"}>
-        //     <li>{status.success ? "false" : "true"}</li>
-        //     <li>{status.status_code}</li>
-        //     <li>{status.status_message}</li>
-        // </ul>
-    <>
-        {status.success ?
-            (<>
-                <Link href={'/movies'}>Sign out</Link>
-                <Link href="/"><AvatarComponent/></Link>
-            </>)
-                :(<>
+        <>
+            {status.success ?
+                (<>
+                    <Link href={'/'}>Sign out</Link>
+                    <Link href={"/movies"}><AvatarComponent/></Link>
+                </>)
+                : (<>
                     <Link href={'/'}>Sign in</Link>
-                    <div className={"w-8 h-8 bg-gray-400 rounded-full border-2 text-black text-center font-bold"}>?</div>
+                    <div className={"w-8 h-8 bg-gray-400 rounded-full border-2 text-black text-center font-bold"}>?
+                    </div>
                 </>)
             }
         </>
     );
 };
-
 export default AuthComponent;
