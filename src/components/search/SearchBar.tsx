2f5d3b6089e3fa898f9ed72ae326c8af3be4762d
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 18:58:25 +0300

    Updated code, fixed bug

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,42 +1,46 @@
 'use client';
-import React from 'react';
+import React, {FC} from 'react';
 import {FaSearch} from 'react-icons/fa';
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import {useDebouncedCallback} from "use-debounce";
 
-const SearchBar = ({ placeholder }: { placeholder: string }) => {
+type Props = {
+    placeholder: string
+}
+const SearchBar:FC<Props> = ({ placeholder}:{placeholder: string}) => {
+    const {replace} = useRouter();
     const searchParams = useSearchParams();
     const pathname = usePathname();
-    const {replace} = useRouter();
     const handleSearch = useDebouncedCallback((word: string) => {
-        const params = new URLSearchParams(searchParams);
-        params.set('page', '1');
-        if (word) {
-            params.set('query', word )
-        } else {
-            params.delete('query');
-        }
-        replace(`${pathname}?${params.toString()}`);
+            const url = new URLSearchParams(searchParams);
+            url.delete('query');
+            url.delete('page');
+            if (word) {
+                url.set('query', word );
+                url.set('page', '1');
+            }
+            replace(`${pathname}?${url.toString()}`);
     }, 300);
     return (
-        <form  className={"w-[600px] relative"}>
+        <form  className={"w-[280px] md:w-[600px] relative"}>
             <label htmlFor="search" className={"sr-only"}> Search </label>
             <div className={"relative"}>
                 <input
-                       className={"w-full text-lg p-4 rounded-full bg-slate-800 placeholder:italic placeholder:text-slate-400 block border border-slate-300  shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"}
+                       className={"w-full text-xl p-4  rounded-full bg-slate-400 placeholder:italic placeholder:text-slate-400 block border border-slate-300  shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm md:text-base"}
                        placeholder={placeholder}
-                       type="text" name="search"
+                       type="text"
                        onChange={(e) => {
+                           e.preventDefault();
                            handleSearch(e.target.value);
                        }}
+                       id="search"
                        defaultValue={searchParams.get('query')?.toString()}
                 />
-                <button className={"invisible sm:visible  absolute right-1 top-1/2 -translate-y-1/2 p-4 rounded-full bg-slate-900"}>
+                <div className={"pl-25 hidden md:block md:visible  md:absolute right-1 top-1/2 -translate-y-1/2 p-4 rounded-full bg-slate-500"}>
                     <FaSearch/>
-                </button>
+                </div>
             </div>
         </form>
     );
 };
-
 export default SearchBar;
