0e9e47ef2e1a098b0c30beb6c3d90b4c636b173b
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 13:05:07 +0300

    Changed styles

--- src/components/MoviesListCard.tsx
+++ src/components/MoviesListCard.tsx
@@ -25,7 +25,8 @@ const MoviesListCard:FC<IProps> = ({infoMovie}) => {
                                width="0"
                                height="0"
                                sizes="100vw"
-                               className={"w-auto h-auto border-4xl border-amber-200 rounded"}/>
+                               className={"w-auto h-auto border-4xl border-amber-200 rounded"}
+                               priority={true}/>
                 </Badge>
                 <div className={"w-full md:w-4/12"}>
                     <div className={"m-2 font-bold text-cyan-500"}>
