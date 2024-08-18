fe87f443870bd940fbf18ab936ae8ad14f16bc9e
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 13:07:40 +0300

    Changed styles

--- src/app/(search)/search/layout.tsx
+++ src/app/(search)/search/layout.tsx
@@ -5,7 +5,7 @@ type Props = {children: React.ReactNode};
 const SearchLayout =  ({children}: Props) => {
     return (
             <div className={"w-full mx-auto content-center flex flex-col items-center"}>
-                <h1 className={"text-6xl text-sky-600"}>Search Movies</h1>
+                <h1 className={"text-6xl text-sky-600 text-center"}>Search Movies</h1>
                 <div className={"flex justify-center m-8"}>
                     <SearchBar placeholder={"Search Movies ..."}/>
                 </div>
