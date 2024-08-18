e5ae35ffb0d26d11415738fb3c90712172627f9f
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 16:32:14 +0300

    Updated  code, changed styles

--- src/app/(search)/search/layout.tsx
+++ src/app/(search)/search/layout.tsx
@@ -4,7 +4,7 @@ import SearchBar from "@/components/search/SearchBar";
 type Props = {children: React.ReactNode};
 const SearchLayout =  ({children}: Props) => {
     return (
-            <div className={"w-full mx-auto content-center flex flex-col items-center"}>
+            <div className={"w-full mx-auto content-center flex flex-col items-center relative min-h-screen"}>
                 <h1 className={"text-6xl text-sky-600 text-center"}>Search Movies</h1>
                 <div className={"flex justify-center m-8"}>
                     <SearchBar placeholder={"Search Movies ..."}/>
--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -2,7 +2,10 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({
     subsets: ['latin'],
-    variable: "--font-sans", });
+    variable: "--font-sans",
+    display: "swap",
+    preload: true
+});
 
 export const lusitana = Lusitana({
     weight: ['400', '700'],
