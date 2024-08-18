462c02de70c15ee16951bf8c9aaf744a87e7a8a5
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 13:07:54 +0300

    Changed styles

--- src/components/AvatarComponent.tsx
+++ src/components/AvatarComponent.tsx
@@ -5,11 +5,10 @@ const AvatarComponent = () => {
     return (
         <div>
             <Avatar>
-                <AvatarImage src="/3d-cartoon-style-character.jpg" alt={"YM"} className={"hover:"}/>
+                <AvatarImage src="/3d-cartoon-style-character.jpg" alt={"YM"} title={"YuliyaM"} className={"hover:tw-class-name hover:opacity-80"}/>
                 <AvatarFallback>YM</AvatarFallback>
             </Avatar>
         </div>
     );
 };
-
 export default AvatarComponent;
