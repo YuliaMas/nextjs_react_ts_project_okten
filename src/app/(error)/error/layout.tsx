6b090b401e94a7f803e6f4978e78349d9bc8c954
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 22:53:12 +0300

    Create Error Page and add styles

--- src/app/(error)/error/layout.tsx
+++ src/app/(error)/error/layout.tsx
@@ -3,8 +3,8 @@ import React from "react";
 type Props = {children: React.ReactNode};
 const ErrorLayout =  ({children}: Props) => {
     return (
-        <div className={"w-full h-full mx-auto content-center flex flex-col items-center mt-20"}>
-            <main>
+        <div className={"w-full mx-auto content-center flex flex-col items-center mt-20"}>
+            <main className={"max-h-screen min-h-screen"}>
                 {children}
             </main>
         </div>
