fe140b3dc172785e0bc4d6987026b8afb551c80a
Yuliya Masechko <yulia.masechko@gmail.com>
2024-08-18 18:57:15 +0300

    Changes styles

--- src/app/(genres)/genres/layout.tsx
+++ src/app/(genres)/genres/layout.tsx
@@ -5,7 +5,7 @@ import GenreComponent from "@/components/GenreComponent";
 export default async function Layout({ children }: { children: React.ReactNode}) {
     const genres = await genreService.getGenres();
     return (
-        <div className="flex  flex-col md:flex-row items-center md:items-start">
+        <div className="flex min-h-screen  flex-col md:flex-row items-center md:items-start">
             <div>
                 <h1 className={"text-2xl mb-3 md:mb-0 md:text-6xl text-sky-600 text-center"}>Genres</h1>
                 <div className="flex w-full flex-wrap md:w-2/12 flex-row md:flex-col pe-6 md:p-12 lg:p-6">
